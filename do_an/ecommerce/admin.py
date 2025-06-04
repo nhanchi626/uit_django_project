ab1a32d76c661c72a5edb9522dd69e7036488e1f
nhanchi626 <132734133+nhanchi626@users.noreply.github.com>
2025-06-04 13:42:19 +0700

    create db

--- do_an/ecommerce/admin.py
+++ do_an/ecommerce/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Role, User, ProductCategory, Product, Order
 # Register your models here.
+
+
+admin.site.register(Role)
+admin.site.register(User)
+admin.site.register(ProductCategory)
+admin.site.register(Product)
+admin.site.register(Order)
