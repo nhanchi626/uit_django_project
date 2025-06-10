b58c987a47f32a15124f2e0848267b1f2a4080eb
tao.duongkhac <tao.duongkhac@gmail.com>
2025-06-10 19:51:10 +0700

    [update] CRUD of product-category and product

--- do_an/ecommerce/urls.py
+++ do_an/ecommerce/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
+
 from . import views
-from .views import RegisterView, CustomTokenObtainView
-from .views import UserListView
-from .views import UserDetailView
+from .views import RegisterView, CustomTokenObtainView, ProductDetailView, UserListView, UserDetailView, \
+    ProductCategoryListView, ProductCategoryDetailView, ProductListView
 
 urlpatterns = [
     path('', views.member_list, name='member_list'),
@@ -12,5 +12,9 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('users/', UserListView.as_view(), name='user_list'),
     path('users/<int:id>/', UserDetailView.as_view(), name='user_detail'),
+    path('categories/', ProductCategoryListView.as_view(), name='categories'),
+    path('products/', ProductListView.as_view(), name='products'),
+    path('categories/<int:id>/', ProductCategoryDetailView.as_view(), name='category'),
+    path('products/<int:id>/', ProductDetailView.as_view(), name='product'),
 ]
 
